5e63e47782a889629a419dcf5525274b2267b797
Monique-creator <m.niyobyose@alustudent.com>
2026-07-12 22:30:47 +0200

    feat: add shared fetch helper for backend requests

---
+++ frontend/js/api.js
@@ -0,0 +1,17 @@
+const API_BASE = "http://localhost:8000";
+
+async function apiPost(path, body) {
+  const res = await fetch(`${API_BASE}${path}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+
+  if (!res.ok) {
+    const err = await res.json().catch(() => ({}));
+    throw new Error(err.detail || "Something went wrong");
+  }
+
+  return res.json();
+}
+
